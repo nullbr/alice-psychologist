f912d481385eff63cff9f44c6d57e1904e2e77c7
Bruno Leite <bmarianoleite3@gmail.com>
2024-10-14 20:52:35 -0300

    hero mobile version

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,15 +6,15 @@ import OnlineMeeting from "../assets/online-meeting.svg";
 const Hero = () => {
   return (
     <section id="hero" className="bg-outline-pattern text-gray-600">
-      <article className="flex flex-col lg:flex-row gap-6 lg:gap-10 text-center md:text-xl -mb-[50svh] max-w-7xl mx-auto h-[100svh] pt-20">
-        <div className="flex flex-col gap-2 sm:gap-4 lg:gap-0 h-full justify-around sm:justify-normal lg:justify-around max-w-[500px] max-h-[500px] m-auto pb-10">
-          <div className="md:h-[50svh] flex flex-col items-center justify-end pb-10">
+      <article className="flex flex-col lg:flex-row gap-6 lg:gap-10 text-center md:text-xl -mb-[40svw] lg:-mb-[50svh] max-w-7xl mx-auto h-[100svh] pt-20">
+        <div className="flex flex-col gap-2 sm:gap-4 lg:gap-0 h-full justify-around max-w-[500px] max-h-[500px] m-auto lg:pb-10 px-6 lg:px-0">
+          <div className="lg:h-[50svh] flex flex-col items-center justify-end lg:pb-10">
             <p className="sm:text-xl md:text-2xl">
               Psicóloga Especializada em Ansiedade e Terapia
               Cognitivo-Comportamental
             </p>
           </div>
-          <div className="md:h-[50svh] flex flex-col items-center justify-start pt-10">
+          <div className="lg:h-[50svh] flex flex-col items-center justify-start lg:pt-10">
             <p className="lg:text-white mb-2 sm:mb-6 sm:text-xl md:text-2xl">
               Ofereço consultas online e presenciais em Ipanema/RJ. Entre em
               contato para agendar uma sessão.
@@ -43,10 +43,10 @@ const Hero = () => {
         <img
           src={HeroImg}
           alt="Alice Araujo"
-          className="rounded-full border border-blue-500 shadow-xl w-[70vw] h-[70vw] md:w-[40svw] md:h-[40svw] md:max-w-[600px] md:max-h-[600px] justify-center items-center text-xl md:text-2xl m-auto mx-auto"
+          className="rounded-full border border-blue-500 shadow-xl w-[80svw] h-[80svw] lg:w-[40svw] lg:h-[40svw] max-w-[500px] max-h-[500px] lg:max-w-[600px] lg:max-h-[600px] justify-center items-center text-xl md:text-2xl m-auto mx-auto mb-2 lg:mb-auto"
         />
       </article>
-      <div className="bg-gray-500 text-center text-white md:text-xl pt-[40vw] md:pt-[50svh] rounded-t-7xl" />
+      <div className="bg-gray-500 text-center text-white md:text-xl pt-[40svw] lg:pt-[50svh] rounded-t-7xl" />
     </section>
   );
 };
